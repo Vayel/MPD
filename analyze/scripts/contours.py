69c76244e6132aeb0da011cc6e07fcd3b6fe0adc
Vayel <vincent.lefoulon@free.fr>
2014-07-03 01:17:44 -0700

    Calculate perimeter

--- analyze/scripts/contours.py
+++ analyze/scripts/contours.py
@@ -11,14 +11,28 @@ from global_functions import ensureDir
 
 def main(path):
   src = loadImg(path)
+  src = cv2.resize(src, (0,0), fx=0.7, fy=0.7)
   gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
   thresh = cv2.threshold(gray, 127, 255, 0)[1]
   
   contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-  cv2.drawContours(src, contours, -1, (255,0,0), 2)
-  cv2.imshow("Contours", src)
-  print str(len(contours)) + " contours."
+  contoursLen = len(contours)
+  plantsNumber = 0
+  colorStep = int(200.0/contoursLen)
+  PERIMETER_LIMIT = 30
+  LINE_WIDTH = 2
   
+  for i in range(contoursLen):
+    perimeter = cv2.arcLength(contours[i], True)
+    if perimeter > PERIMETER_LIMIT:
+      plantsNumber += 1
+      val = (i+1) * colorStep
+      cv2.drawContours(src, [contours[i]], -1, (val,val,val), LINE_WIDTH)
+      print "(" + str(val) + "," + str(val) + "," + str(val) + ") : " + str(perimeter)
+  
+  print "\n" + str(plantsNumber) + " plants."
+  
+  cv2.imshow("Contours", src)
   cv2.waitKey()
   cv2.destroyAllWindows()
 
